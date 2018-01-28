69b254aca083079a8b9ad1dad0fa796fca1b5c0f
Tommy Spielhoelle <spiel.hoelle@live.de>
2018-01-28 20:02:31 +0100

    add rest routes for bears

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,12 @@
 module.exports = function(router){
+  // middleware to use for all requests
+  router.use(function(req, res, next) {
+    // do logging
+    //console.log("response",res);
+    //console.log("request",req);
+    next(); // make sure we go to the next routes and don't stop here
+  });  
+
   router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
   });
